3ddbbd3f3b530f7bba659db898214fcfef6af629
benjamin2300 <benjamin2300.b98902064@gmail.com>
2018-07-29 00:37:16 +0800

    Problem : Maximum Subarray
    Easy

--- Maximum_Subarray.py
+++ Maximum_Subarray.py
@@ -8,7 +8,6 @@ class Solution(object):
         record = [0] * len(nums)
         record[0] = nums[0]
         for i in range(1, len(nums)):
-            record[i] = nums[i]
             record[i] = max(record[i-1]+nums[i], nums[i])
         print record
         return max(record)
