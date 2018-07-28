b848601baded53a7d4e5c8a2755c6d59d7a9b644
benjamin2300 <benjamin2300.b98902064@gmail.com>
2018-07-29 00:34:07 +0800

    Problem :  Maximum Subarray
    Easy

---
+++ Maximum_Subarray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        summon = 0
+        record = [0] * len(nums)
+        record[0] = nums[0]
+        for i in range(1, len(nums)):
+            record[i] = nums[i]
+            record[i] = max(record[i-1]+nums[i], nums[i])
+        print record
+        return max(record)
+                
